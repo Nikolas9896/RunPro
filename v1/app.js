cc917924e52855b9326482661e503f64c452e649
Nikolas9896 <nhlynka@gmail.com>
2019-09-13 15:58:38 +0300

    push new race to races list and move out from races array to global scope

--- v1/app.js
+++ v1/app.js
@@ -5,23 +5,29 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var races = [
+    {name: "Half Marathon", image: "https://runstyle.net/wp-content/uploads/2017/12/race_nation_400-257x257.png"},
+    {name: "10th Kilometers", image: "https://runstyle.net/wp-content/uploads/2018/05/npchernigiv-260x260.png"},
+    {name: "Run by Run", image: "https://runstyle.net/wp-content/uploads/2019/08/1200h500px-1200x480.jpg"}
+];
+
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/races", (req, res) => {
-    var races = [
-        {name: "Half Marathon", image: "https://runstyle.net/wp-content/uploads/2017/12/race_nation_400-257x257.png"},
-        {name: "10th Kilometers", image: "https://runstyle.net/wp-content/uploads/2018/05/npchernigiv-260x260.png"},
-        {name: "Run by Run", image: "https://runstyle.net/wp-content/uploads/2019/08/1200h500px-1200x480.jpg"}
-    ]
+   
     res.render("races", {races: races});
 });
 
 app.post("/races", (req, res) => {
     //get data from form and add to races array
-    res.send("You hit The post Route!");
+    var name = req.body.name;
+    var image = req.body.image;
+    var newRaces = {name: name, image: image}
+    races.push(newRaces);
     //redirect to races
+    res.redirect("/races");
 });
 
 app.get("/races/new", (req, res) => {
