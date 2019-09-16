92ec66da477c13e75cb72ea0be05b6d009beb66e
Nikolas9896 <nhlynka@gmail.com>
2019-09-16 22:36:46 +0300

    change connect to server link and useNewUrlParser: true

--- v2/app.js
+++ v2/app.js
@@ -3,7 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose");
 
-mongoose.connect("mongodb://localhost/run_pro");
+mongoose.connect("mongodb://localhost:27017/run_pro", {useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -50,9 +50,17 @@ app.post("/races", (req, res) => {
     var name = req.body.name;
     var image = req.body.image;
     var newRaces = {name: name, image: image}
-    races.push(newRaces);
-    //redirect to races
-    res.redirect("/races");
+    //Create a new Race and save to DB
+    Race.create(newRaces, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect to races
+            res.redirect("/races");
+        }
+    });
+    
+   // res.redirect("/races");
 });
 
 app.get("/races/new", (req, res) => {
