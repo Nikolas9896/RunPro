5a2309edec0664cc19455049da7714fecb0da615
Nikolas9896 <nhlynka@gmail.com>
2019-09-24 11:15:49 +0300

    create v5 for styling Comment page

---
+++ v5/models/race.js
@@ -0,0 +1,16 @@
+//SCHEMA RACE
+var mongoose    =require("mongoose");
+
+var raceSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+module.exports = mongoose.model("Race", raceSchema);
