4189e932b554d0eac819b3cfd5cc821373a2038d
Nikolas9896 <nhlynka@gmail.com>
2019-09-26 08:57:43 +0300

    setup passport configuration

--- v6/app.js
+++ v6/app.js
@@ -1,17 +1,35 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Race        = require("./models/race"),
-    Comment     = require("./models/comment"),
-    // User        = require("./models/user"),
-    seedDB      = require("./seeds");
+var express                 = require("express"),
+    app                     = express(),
+    bodyParser              = require("body-parser"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    LocalStrategy           = require("passport-local"),
+    Race                    = require("./models/race"),
+    Comment                 = require("./models/comment"),
+    User                    = require("./models/user"),
+    seedDB                  = require("./seeds");
 //seedDB - function for create DEMO RACE.
 //seedDB();
 mongoose.connect("mongodb://localhost:27017/run_pro", {useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+// PASSPORT CONFIGURATION
+
+app.use(require("express-session")({
+    secret: "My son is the best and the cutest man in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 //ROUTES
 app.get("/", (req, res) => {
