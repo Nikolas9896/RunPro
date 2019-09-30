7f399a878f783a69bd8035620400a19e6a535549
Nikolas9896 <nhlynka@gmail.com>
2019-09-30 20:45:35 +0300

    include flash message for login route

--- v11/routes/index.js
+++ v11/routes/index.js
@@ -35,7 +35,7 @@ router.post("/register", (req, res) => {
 
 //show login form
 router.get("/login", (req, res) => {
-    res.render("login");
+    res.render("login", {message: req.flash("error")});
 });
 
 //handling login logc
