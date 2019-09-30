78f60f963038314ada3759341593c5d1e9120490
Nikolas9896 <nhlynka@gmail.com>
2019-09-30 21:17:07 +0300

    add success flash message for logout and error message for login page

--- v11/routes/index.js
+++ v11/routes/index.js
@@ -35,7 +35,7 @@ router.post("/register", (req, res) => {
 
 //show login form
 router.get("/login", (req, res) => {
-    res.render("login", {message: req.flash("error")});
+    res.render("login");
 });
 
 //handling login logc
@@ -49,6 +49,7 @@ router.post("/login", passport.authenticate("local", {
 // logout route
 router.get("/logout", (req, res) => {
     req.logout();
+    req.flash("success", "Logged you out!");
     res.redirect("/races");
 });
 
